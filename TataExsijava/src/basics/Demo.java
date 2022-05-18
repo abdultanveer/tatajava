c165b11af66a2b58f884ae563476e4caae4e2bf0
abdul <abdul.tanveer@gmail.com>
2022-05-18 10:26:39 +0530

    initial commit

---
+++ TataExsijava/src/basics/Demo.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class Demo {
+	public static void main(String[] args) {
+		Student abdul = new Student(); //new -- allocate memory in RAM -- create a student box
+		abdul.calculateElgibility();
+	}
+
+}
---
+++ TataExsijava/src/basics/Student.java
@@ -0,0 +1,14 @@
+package basics;
+
+
+class Student{
+	String name; //partition -- instance variable
+	int age;
+	
+	
+	//function -- method
+	void calculateElgibility() {
+		int a = 20; //a is my local variable
+		System.out.println("abdul's marks are--"+a);
+	}
+}
